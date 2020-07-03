9eb6de4568c29abce481cbc673c21628ff8aef97
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-02 18:01:21 -0700

    Updates

---
+++ precache-manifest.41e2da09a67e75ee44be643fa151a4b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98da13b1bc8b93a4beb1324aacca23f2",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "782e2902daef2bc6b809",
+    "url": "/to-do-app/static/js/2.c4fff775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.c4fff775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3007a66ae3a1dc4151",
+    "url": "/to-do-app/static/js/main.473ce058.chunk.js"
+  },
+  {
+    "revision": "d36d282d638c6db65e35",
+    "url": "/to-do-app/static/js/runtime-main.c555a6ca.js"
+  }
+]);
